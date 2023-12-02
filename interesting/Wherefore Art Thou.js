09080bae7afb7be34fc255fd7e1a7a2509e16272
Vic Samoylik <v.samojlik@gmail.com>
2023-12-02 22:17:20 +0300

    feat: solve wherefore art thou challenge

---
+++ interesting/Wherefore Art Thou.js
@@ -0,0 +1,34 @@
+// Wherefore art thou
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+
+// function whatIsInAName(collection, source) {
+//     const sourceKeys = Object.keys(source)
+//     return collection.filter((item) => {
+//         for(let i = 0; i < sourceKeys.length; i++) {
+//             if(item[sourceKeys[i]] !== source[sourceKeys[i]]) {
+//                 return false
+//             }
+//         }
+//         return true
+//     })
+// }
+
+function whatIsInAName(collection, source) {
+    return collection.filter((item) => {
+        for(let property in source) {
+            if(item[property] !== source[property]) {
+                return false
+            }
+        }
+        return true
+    })
+}
+
+
+// console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })) // should return [{ first: "Tybalt", last: "Capulet" }]
+// console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 }))// should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }]
+// console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))// should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]
+// console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }))// should return [{ "apple": 1, "bat": 2, "cookie": 2 }]
+// console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }))// should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }]
