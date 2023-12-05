4c8e6da6117062448b36bbca971ca6eb1f810abb
Vic Samoylik <v.samojlik@gmail.com>
2023-12-06 02:34:23 +0300

    feat: solve cash register

---
+++ interesting/Cash Register.js
@@ -0,0 +1,81 @@
+// Cash Register
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+// cid is a 2D array listing available currency.
+// The checkCashRegister() function) // should always return an object with a status key and a change key.
+// Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+// Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+// Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+// See below for an example of a cash-in-drawer array:
+
+// [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100]
+// ]
+
+
+// Decide to use nums (in cents) instead of floats to get more correct calculations ( it's JS.. =) )
+const dictionary = {
+    "PENNY": 1,
+    "NICKEL": 5,
+    "DIME": 10,
+    "QUARTER": 25,
+    "ONE": 100,
+    "FIVE": 500,
+    "TEN": 1000,
+    "TWENTY": 2000,
+    "ONE HUNDRED": 10000
+}
+
+function checkCashRegister(price, cash, cid) {
+    let sumChange = cash * 100 - price * 100;
+    let sumChangeOriginal = sumChange
+    let change = []
+    let status = ''
+
+    let cidTotal = 0
+    // Exclude 0 values in cid and sort them from bigger to lower
+    let filteredCid = cid.filter(item=> item[1] !== 0).reverse()
+
+    filteredCid.forEach(item => {
+        let currency = item[0]
+        let currencySum = item[1] * 100
+        cidTotal += currencySum
+        let amount = 0
+        while(sumChange >= dictionary[currency] && currencySum > 0) {
+            amount += dictionary[currency]
+            sumChange -= dictionary[currency]
+            currencySum -= dictionary[currency]
+        }
+        if(amount !== 0) {
+            change.push([currency, amount / 100])
+        }
+    })
+
+    if(sumChange > 0) {
+        status = "INSUFFICIENT_FUNDS"
+        change = []
+    } else if(sumChange == 0 && sumChangeOriginal == cidTotal) {
+        status = "CLOSED"
+        change = cid
+    } else {
+        status = "OPEN"
+    }
+    return ({
+        "status": status,
+        "change": change
+    })
+}
+
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) // should return an object.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) // should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])) // should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])) // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
